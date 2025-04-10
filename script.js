78f4cfde03e3dc6ca51bb4f62edce85097e81a1f
gustavo <gustavo_klahold@estudante.sesisenai.org.br>
2025-04-10 09:34:54 -0300

    add

--- script.js
+++
@@ -1,9 +0,0 @@
-function validarLogin() {
-
-    const Email = document.getElementById('Email_maquinista').value.trim();
-
-    const Codigo = document.getElementById('Codigo_maquinista').value.trim();
-
-    const Senha = document.getElementById('Senha_maquinista').value.trim();
-
-}
